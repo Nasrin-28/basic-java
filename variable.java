fd122b23210169efbde3064cdedda6cfdcbbb810
Nasrin-28 <157484228+Nasrin-28@users.noreply.github.com>
2024-02-08 12:04:33 +0530

    Add files via upload
    add variables

---
+++ variable.java
@@ -0,0 +1,21 @@
+//04 variables in java
+
+import java.lang.*;
+
+class variables
+{
+	public static void main(String args[])
+	{
+		String name="Nasrin";
+		int age=25;
+		float percent=75.66f;
+		char gender='F';
+		boolean married=true;
+		
+		System.out.println("Name    :"+name);
+		System.out.println("Age :"+age);
+		System.out.println("Percent    :"+percent);
+		System.out.println("Gender    :"+gender);
+		System.out.println("Married    :"+married);
+	}
+}
