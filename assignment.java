e257757566cabf5f727a051c290fdb493d1eb1e0
Nasrin-28 <157484228+Nasrin-28@users.noreply.github.com>
2024-02-10 22:10:30 +0530

    Add files via upload
    add assignment program

---
+++ assignment.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+public class assignment
+{
+	/*
+	Arithmetic assignment operator
+	+= 	a+=10	a=a+10
+	-=	a-=10	a=a-10
+	*=	a*=10	a=a*10
+	/=	a/=10	a=a/10
+	%=	a%=10	a=a%10
+	*/
+	public static void main(String args[])
+	
+	{
+	int a=123;
+	System.out.println(a);
+	
+	a+=10;
+	System.out.println(a);
+	
+	a-=10;
+	System.out.println(a);
+	
+	a*=10;
+	System.out.println(a);
+	
+	a/=10;
+	System.out.println(a);
+	
+	a%=10;
+	System.out.println(a);
+	
+	}
+}
+
+	
