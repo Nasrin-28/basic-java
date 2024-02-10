1d82a2b398dadb4b71224f197b104f9e901cfe86
Nasrin-28 <157484228+Nasrin-28@users.noreply.github.com>
2024-02-10 19:57:56 +0530

    Add files via upload
    add casting program

---
+++ casting.java
@@ -0,0 +1,28 @@
+//04 Type casting in java
+/*
+  widening casting
+ 
+        byte -> short -> char -> int -> float -> long -> double
+		
+	Narrowing casting
+	
+		double -> long -> float-> int -> char -> short -> byte
+*/
+import java.lang.*;
+
+class casting
+{
+public static void main(String args[])
+
+
+{
+	int a=10;
+	double b=a;
+	
+	System.out.println("int  :"+a);
+	System.out.println("double :"+b);
+	
+}
+}
+		
+  
