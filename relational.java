af8eac290a2a256b7688267b45bdc2488a437a64
Nasrin-28 <157484228+Nasrin-28@users.noreply.github.com>
2024-02-10 22:29:57 +0530

    Add files via upload
    add relational operators

---
+++ relational.java
@@ -0,0 +1,25 @@
+//relational operator
+import java.lang.*;
+public class relational
+{
+	/*
+	operator	name						example
+	==			Equal to					a==b
+	!=			Not equal					a!=b
+	>			Greater than				a>b
+	<			Less than					a<b
+	>=			Greater than or equal to	a>=b
+	<=			Less than or equal to 		a<=b
+	*/
+	public static void main(String args[])
+	{
+		int a=500,b=250;
+		System.out.println("Equal to :"	+(a==b));
+		System.out.println("Not Equal to :"	+(a!=b));
+		System.out.println("Greater than :"	+(a>b));
+		System.out.println("Less than:"	+(a<b));
+		System.out.println("Greater than or Equal to :"	+(a>=b));
+		System.out.println("Less than or Equal to :"	+(a<=b));
+	}
+}
+		
