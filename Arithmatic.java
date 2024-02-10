e94dca4462b23850a8ffbaa59eb917cff150da8b
Nasrin-28 <157484228+Nasrin-28@users.noreply.github.com>
2024-02-10 19:25:08 +0530

    Add files via upload
    add arithmatic operators

---
+++ Arithmatic.java
@@ -0,0 +1,13 @@
+//Arithmatic operator
+public class Arithmatic
+{
+	public static void main(String args[])
+	{
+		int a=123,b=10;
+		System.out.println("Addtion : "+(a+b));
+		System.out.println("Subraction : "+(a-b));
+		System.out.println("Multiplication : "+(a*b));
+		System.out.println("Division : "+(a/b));
+		System.out.println("Modulus : "+(a%b));
+}
+}
